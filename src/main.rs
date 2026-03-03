aa81afeeefbdd150dcb7ca0068e681052c852325
lycopersicum-t <anastasijanovik06@gmail.com>
2026-03-03 23:32:05 +0300

    main db is connected, need to hide link

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+mod entities;
+use sea_orm::{Database, DbErr, EntityTrait, ActiveModelTrait, ActiveValue::Set};
+use entities::{category, task};
+
+fn add_category() {
+
+}
+
+#[tokio::main]
+async fn main() -> Result<(), DbErr> {
+    let db_url = "postgres://myuser:5521@127.0.0.1:5433/tododb";
+    let db = Database::connect(db_url).await?;
+    println!("Соединение с базой установлено!");
+    Ok(())
 }
